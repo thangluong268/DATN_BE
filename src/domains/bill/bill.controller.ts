2db1420eb308736d83ee43556e21a26f3251585b
Myth <luongthangg268@gmail.com>
2024-03-03 02:01:53 +0700

    feature: create, countTotalByStatusSeller - Bill

--- src/domains/bill/bill.controller.ts
+++ src/domains/bill/bill.controller.ts
@@ -1,19 +1,26 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { Roles } from 'domains/auth/decorators/auth-role.decorator';
 import { AuthJwtATGuard } from 'domains/auth/guards/auth-jwt-at.guard';
 import { ROLE_NAME } from 'shared/enums/role-name.enum';
 import { BillService } from './bill.service';
 import { BillCreateREQ } from './request/bill-create.request';
+import { BillGetTotalByStatusSellerREQ } from './request/bill-get-total-by-status-seller.request';
 
 @Controller('bill')
 export class BillController {
   constructor(private readonly billService: BillService) {}
 
+  @Roles(ROLE_NAME.SELLER)
+  @UseGuards(AuthJwtATGuard)
+  @Get('seller/count-total-by-status')
+  countTotalByStatusSeller(@Req() req, @Query() query: BillGetTotalByStatusSellerREQ) {
+    return this.billService.countTotalByStatusSeller(req.user._id, Number(query.year));
+  }
+
   @Roles(ROLE_NAME.USER)
   @UseGuards(AuthJwtATGuard)
   @Post('user')
   create(@Req() req, @Body() body: BillCreateREQ) {
-    const userId = req.user._id;
-    return this.billService.create(userId, body);
+    return this.billService.create(req.user._id, body);
   }
 }
--- src/domains/bill/bill.service.ts
+++ src/domains/bill/bill.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { CartService } from 'domains/cart/cart.service';
 import { ProductService } from 'domains/product/product.service';
+import { StoreService } from 'domains/store/store.service';
 import { UserService } from 'domains/user/user.service';
-import { Model } from 'mongoose';
+import { Connection, Model } from 'mongoose';
+import { BILL_STATUS } from 'shared/constants/bill.constant';
+import { BaseResponse } from 'shared/generics/base.response';
+import { toDocModel } from 'shared/helpers/to-doc-model.helper';
+import { ProductInfoDTO } from './dto/product-info.dto';
 import { BillCreateREQ } from './request/bill-create.request';
+import { BillGetTotalByStatusSellerREQ } from './request/bill-get-total-by-status-seller.request';
 import { Bill } from './schema/bill.schema';
 
 @Injectable()
@@ -12,32 +18,57 @@ export class BillService {
   constructor(
     @InjectModel(Bill.name)
     private readonly billModel: Model<Bill>,
+    @InjectConnection()
+    private readonly connection: Connection,
 
     private readonly userService: UserService,
     private readonly productService: ProductService,
     private readonly cartService: CartService,
+    private readonly storeService: StoreService,
   ) {}
 
   async create(userId: string, body: BillCreateREQ) {
     const user = await this.userService.findById(userId);
     if (!user) return new NotFoundException('Không tìm thấy người dùng này!');
-    for (const bill of body.data) {
-      await this.userService.updateWallet(userId, bill.totalPrice, 'plus');
-      await this.cartService.removeMultiProductInCart(userId, bill.storeId, bill.products);
-      for (const product of bill.products) {
-        await this.productService.decreaseQuantity(product.id, product.quantity);
+    const newBills = [];
+    for (const cart of body.data) {
+      const session = await this.connection.startSession();
+      session.startTransaction();
+      try {
+        await this.userService.updateWallet(userId, cart.totalPrice, 'plus');
+        await this.cartService.removeMultiProductInCart(userId, cart.storeId, cart.products);
+        for (const product of cart.products) {
+          await this.productService.decreaseQuantity(product.id, product.quantity);
+        }
+        cart.products.forEach((product: ProductInfoDTO) => {
+          product.type = product.type.toUpperCase();
+        });
+        const newBill = await this.billModel.create(cart);
+        BillCreateREQ.saveData(newBill, userId, body);
+        newBills.push(newBill);
+      } catch (err) {
+        await session.abortTransaction();
+        throw err;
       }
-      //   const newBill = await this.billModel.create(
-      //     userId,
-      //     bill,
-      //     createBillDto.deliveryMethod,
-      //     createBillDto.paymentMethod,
-      //     createBillDto.receiverInfo,
-      //     createBillDto.giveInfo,
-      //     createBillDto.deliveryFee,
-      //   );
-
-      //   return newBill;
     }
+    return newBills.map((bill) => toDocModel(bill));
+  }
+
+  async countTotalByStatusSeller(userId: string, year: number) {
+    const user = await this.userService.findById(userId);
+    if (!user) return new NotFoundException('Không tìm thấy người dùng này!');
+    const store = await this.storeService.findByUserId(userId);
+    if (!store) return new NotFoundException('Không tìm thấy cửa hàng này!');
+    const statusData: string[] = BILL_STATUS.split('-').map((item: string) => item.toUpperCase());
+    const countTotal = await Promise.all(
+      statusData.map(async (status: string) => {
+        return this.billModel.countDocuments(BillGetTotalByStatusSellerREQ.toQueryCondition(store._id, status, year));
+      }),
+    );
+    const transformedData = Object.fromEntries(countTotal.map((value, index) => [statusData[index], value]));
+    return BaseResponse.withMessage<{ [k: string]: number }>(
+      transformedData,
+      'Lấy tổng số lượng các đơn theo trạng thái thành công!',
+    );
   }
 }
