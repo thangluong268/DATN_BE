0d2f7554d37ccb95d19de84164057317cea0e9d2
Myth <luongthangg268@gmail.com>
2024-05-05 14:57:32 +0700

    feature: get finances

--- src/domains/finance/finance.controller.ts
+++ src/domains/finance/finance.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
+import { Roles } from 'domains/auth/decorators/auth-role.decorator';
+import { AuthJwtATGuard } from 'domains/auth/guards/auth-jwt-at.guard';
+import { AuthRoleGuard } from 'domains/auth/guards/auth-role.guard';
+import { ROLE_NAME } from 'shared/enums/role-name.enum';
 import { FinanceService } from './finance.service';
 
 @Controller('finances')
 export class FinanceController {
   constructor(private readonly financeService: FinanceService) {}
+
+  @Roles(ROLE_NAME.ADMIN)
+  @UseGuards(AuthJwtATGuard, AuthRoleGuard)
+  @Get()
+  getFinances(@Query('year', ParseIntPipe) year: number) {
+    return this.financeService.getFinances(year);
+  }
 }
--- src/domains/finance/finance.service.ts
+++ src/domains/finance/finance.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { getMonthRevenue } from 'domains/bill/helper/get-month-revenue.helper';
 import { Model } from 'mongoose';
+import { BaseResponse } from 'shared/generics/base.response';
 import { Finance } from './schema/finance.schema';
 
 @Injectable()
@@ -10,4 +12,28 @@ export class FinanceService {
     @InjectModel(Finance.name)
     private readonly financeModel: Model<Finance>,
   ) {}
+
+  async getFinances(year: number) {
+    this.logger.log('Get finances');
+    const data = await this.financeModel.aggregate([
+      {
+        $match: { createdAt: { $gte: new Date(year, 0, 1), $lt: new Date(year + 1, 0, 1) } },
+      },
+      {
+        $group: {
+          _id: { $month: '$createdAt' },
+          expense: { $sum: '$expense' },
+          revenue: { $sum: '$revenue' },
+        },
+      },
+    ]);
+    const monthlyExpense = getMonthRevenue();
+    const monthlyRevenue = getMonthRevenue();
+    data.forEach((entry: { _id: number; expense: number; revenue: number }) => {
+      const month = entry._id;
+      monthlyExpense[`Tháng ${month}`] = entry.expense;
+      monthlyRevenue[`Tháng ${month}`] = entry.revenue;
+    });
+    return BaseResponse.withMessage({ monthlyExpense, monthlyRevenue }, 'Lấy dữ liệu thành công');
+  }
 }
