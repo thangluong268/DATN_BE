fd426b3fd4babdc2b60331cae03016f0a11b7a19
Myth <luongthangg268@gmail.com>
2024-05-18 22:36:22 +0700

    fix: product

--- src/domains/product/product.service.ts
+++ src/domains/product/product.service.ts
@@ -409,7 +409,7 @@ export class ProductService {
 
   async getProductById(id: string) {
     this.logger.log(`Get Product By Id: ${id}`);
-    const product = await this.productModel.findById(id, { status: true }).lean();
+    const product = await this.productModel.findById(id).lean();
     if (!product) throw new NotFoundException('Không tìm thấy sản phẩm này!');
     const type = product.newPrice === 0 ? PRODUCT_TYPE.GIVE : PRODUCT_TYPE.SELL;
     const quantityDelivered = await this.billService.countProductDelivered(id, type, BILL_STATUS.DELIVERED);
